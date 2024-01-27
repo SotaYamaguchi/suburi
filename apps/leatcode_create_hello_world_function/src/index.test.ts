4dee1eebee118793b8513aeb58ff4007d9065581
SotaYamaguchi <sota.yamaguchi@roxx.co.jp>
2024-01-28 00:41:17 +0900

    Hello Worldを返す関数を返すことのテストが通るように

--- apps/leatcode_create_hello_world_function/src/index.test.ts
+++ apps/leatcode_create_hello_world_function/src/index.test.ts
@@ -1,14 +1,17 @@
 // eslint-disable-next-line import/no-unresolved
 import { describe, expect, test } from "bun:test";
+import { createHelloWorld } from "./index.js";
 
 describe("createHelloWorld", () => {
-  test("Hellow Worldを返す関数を返すこと", () => {
+  test("Hello Worldを返す関数を返すこと", () => {
     const f = createHelloWorld();
     expect(f()).toBe("Hello World");
   });
 
   test("任意の引数が渡ってきた場合でも、必ずHello Worldを返すこと", () => {
-    expect(true).toBe(true);
+    const args = [{}, null, 42];
+    const f = createHelloWorld();
+    expect(f(args)).toBe("Hello World");
   });
 
   test("引数の数は最大10個まで渡せること", () => {
---
+++ apps/leatcode_create_hello_world_function/src/index.ts
@@ -0,0 +1,5 @@
+export const createHelloWorld = () => {
+  return () => {
+    return "Hello World";
+  };
+};
