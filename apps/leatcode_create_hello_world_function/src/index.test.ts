a7105d17c782330b3bb8fe22fa916a6598b418e0
SotaYamaguchi <sota.yamaguchi@roxx.co.jp>
2024-01-28 01:03:27 +0900

    引数が11個以上だった場合にエラーとなるテストを追加

--- apps/leatcode_create_hello_world_function/src/index.test.ts
+++ apps/leatcode_create_hello_world_function/src/index.test.ts
@@ -8,13 +8,30 @@ describe("createHelloWorld", () => {
     expect(f()).toBe("Hello World");
   });
 
-  test("任意の引数が渡ってきた場合でも、必ずHello Worldを返すこと", () => {
+  test("任意の引数が渡ってきた場合でも、Hello Worldを返すこと", () => {
     const args = [{}, null, 42];
     const f = createHelloWorld();
     expect(f(args)).toBe("Hello World");
   });
 
-  test("引数の数は最大10個まで渡せること", () => {
-    expect(true).toBe(true);
+  test("引数ˆが0個の場合、Hello Worldを返すこと", () => {
+    const args = [];
+    const f = createHelloWorld();
+    expect(args.length).toBe(0);
+    expect(f(args)).toBe("Hello World");
+  });
+
+  test("引数が10個の場合、Hello Worldを返すこと", () => {
+    const args = Array.from({ "length": 10 }, (_, index) => index);
+    const f = createHelloWorld();
+    expect(args.length).toBe(10);
+    expect(f(args)).toBe("Hello World");
+  });
+
+  test("引数が11個の場合、エラーを投げること", () => {
+    const args = Array.from({ "length": 11 }, (_, index) => index);
+    const f = createHelloWorld();
+    expect(args.length).toBe(11);
+    expect(f(args)).toThrow();
   });
 });
