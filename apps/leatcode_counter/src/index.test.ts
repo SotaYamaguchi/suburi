90193db6c90c1a8c032ff43c178f645c556b17c5
SotaYamaguchi <sota.yamaguchi@roxx.co.jp>
2024-01-28 16:23:48 +0900

    counter問題の完了

--- apps/leatcode_counter/src/index.test.ts
+++ apps/leatcode_counter/src/index.test.ts
@@ -3,44 +3,58 @@ import { describe, expect, test } from "bun:test";
 import { createCounter } from "./index.js";
 
 describe("createCounter", () => {
-  test("数値nを渡した場合、初回呼び出し時にnを返却する関数を返すこと", () => {
-    const f = createCounter();
-    expect(true).toBe(true);
-  });
-
-  test("数値nを渡した場合、返却された関数を1度実行した際にnの値+1した値を返すこと", () => {
-    expect(true).toBe(true);
-  });
-
-  test("数値nを渡した場合、返却された関数を2度実行した際にnの値+2した値を返すこと", () => {
-    expect(true).toBe(true);
-  });
+  test.each([
+    [10, 1, 10],
+    [10, 2, 11],
+    [10, 3, 12],
+  ])(
+    "数値%iを引数に渡した場合、%i回関数を実行すると%iを返すこと",
+    (number, executionCount, expected) => {
+      const f = createCounter(number);
+      const counts = Array.from({ "length": executionCount }, () => {
+        return f();
+      });
+      expect(counts[counts.length - 1]).toBe(expected);
+    },
+  );
 
   describe("引数nの境界値テスト", () => {
-    test("数値nに-1000を渡した場合、返却された関数を実行すると-1000を返すこと", () => {
-      expect(true).toBe(true);
-    });
+    test(
+      "数値nに-1000を渡した場合、返却された関数を実行すると-1000を返すこと",
+      () => {
+        const f = createCounter(-1000);
+        expect(f()).toBe(-1000);
+      },
+    );
 
     test("数値nに-1001を渡した場合、エラーを投げること", () => {
-      expect(true).toBe(true);
+      expect(() => createCounter(-1001)).toThrow();
     });
 
-    test("数値nに1000を渡した場合、返却された関数を実行すると1000を返すこと", () => {
-      expect(true).toBe(true);
-    });
+    test(
+      "数値nに1000を渡した場合、返却された関数を実行すると1000を返すこと",
+      () => {
+        const f = createCounter(1000);
+        expect(f()).toBe(1000);
+      },
+    );
 
     test("数値nに1001を渡した場合、エラーを投げること", () => {
-      expect(true).toBe(true);
+      expect(() => createCounter(1001)).toThrow();
     });
   });
 
   describe("返却した関数の実行回数制限の境界値テスト", () => {
-    test("数値nを渡した場合、返却された関数を実行すると1000回までインクリメントした数を返すこと", () => {
-      expect(true).toBe(true);
-    });
-
-    test("数値nを渡した場合、返却された関数を1001回実行するとエラーを投げること", () => {
-      expect(true).toBe(true);
-    });
+    test(
+      "数値nを渡した場合、返却された関数を1001回実行するとエラーを投げること",
+      () => {
+        const f = createCounter(10);
+        const counts = Array.from({ "length": 1000 }, () => {
+          return f();
+        });
+        expect(counts[counts.length - 1]).toBe(1009);
+        expect(() => f()).toThrow();
+      },
+    );
   });
 });
--- apps/leatcode_counter/src/index.ts
+++ apps/leatcode_counter/src/index.ts
@@ -1,4 +1,20 @@
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const createCounter = () => {
-  return 'hoge'
+const maxNumber = 1000;
+const minNumber = -1000;
+const maxExecutionLimit = 1000;
+
+export const createCounter = (num: number) => {
+  if (num < minNumber || num > maxNumber) {
+    throw new Error();
+  }
+
+  let callCount = 0;
+  return () => {
+    const result = num + callCount;
+    callCount++;
+    if (callCount > maxExecutionLimit) {
+      throw new Error();
+    }
+
+    return result;
+  };
 };
