4f3233e0dfe875a19bd34f8d40e9b89add56579b
SotaYamaguchi <sota.yamaguchi@roxx.co.jp>
2024-01-28 15:23:05 +0900

    counter問題のTODOリスト作成

--- apps/leatcode_counter/src/index.test.ts
+++ apps/leatcode_counter/src/index.test.ts
@@ -1,10 +1,46 @@
 // eslint-disable-next-line import/no-unresolved
 import { describe, expect, test } from "bun:test";
-import { createHelloWorld } from "./index.js";
+import { createCounter } from "./index.js";
 
 describe("createCounter", () => {
-  test("Hello Worldを返す関数を返すこと", () => {
-    const f = createHelloWorld();
-    expect(f()).toBe("Hello World");
+  test("数値nを渡した場合、初回呼び出し時にnを返却する関数を返すこと", () => {
+    const f = createCounter();
+    expect(true).toBe(true);
+  });
+
+  test("数値nを渡した場合、返却された関数を1度実行した際にnの値+1した値を返すこと", () => {
+    expect(true).toBe(true);
+  });
+
+  test("数値nを渡した場合、返却された関数を2度実行した際にnの値+2した値を返すこと", () => {
+    expect(true).toBe(true);
+  });
+
+  describe("引数nの境界値テスト", () => {
+    test("数値nに-1000を渡した場合、返却された関数を実行すると-1000を返すこと", () => {
+      expect(true).toBe(true);
+    });
+
+    test("数値nに-1001を渡した場合、エラーを投げること", () => {
+      expect(true).toBe(true);
+    });
+
+    test("数値nに1000を渡した場合、返却された関数を実行すると1000を返すこと", () => {
+      expect(true).toBe(true);
+    });
+
+    test("数値nに1001を渡した場合、エラーを投げること", () => {
+      expect(true).toBe(true);
+    });
+  });
+
+  describe("返却した関数の実行回数制限の境界値テスト", () => {
+    test("数値nを渡した場合、返却された関数を実行すると1000回までインクリメントした数を返すこと", () => {
+      expect(true).toBe(true);
+    });
+
+    test("数値nを渡した場合、返却された関数を1001回実行するとエラーを投げること", () => {
+      expect(true).toBe(true);
+    });
   });
 });
